8f4d5b76ea70092f7ddb0cbf4e65a3b3f9fe8e3a
Konstantin <freidlinks@ya.ru>
2020-10-26 12:42:50 +0300

    add flake8. Prettify code with linter. more: https://ru.hexlet.io/projects/49/members/11448 task: flake8

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import brain_games.scripts.cli as cli
 
+
 def main():
-	username = cli.welcome_user()
-	# cli.welcome_user()
+    username = cli.welcome_user()
 
 
 if __name__ == "__main__":
-	main()
+    main()
